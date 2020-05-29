c9d61af677a944f3990030e98fb5dd97f6cf5560
Pavel Grebeniuk <grebeniuk.p@gmail.com>
2020-05-29 14:32:54 +0300

    add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,29 @@ document.querySelector('.carousel__next-btn')
   .addEventListener('click', function () {
     slider.goTo('next');
   });
+
+
+const links = document.querySelectorAll('.product__link');
+links.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const parent = e.target.closest('.product__wrap');
+    parent.querySelector('.product__front').classList.toggle('product__front_active');
+    parent.querySelector('.product__back').classList.toggle('product__back_active');
+  });
+});
+
+
+const tabs = document.querySelectorAll('.catalog__tabs-item');
+tabs.forEach(tab => {
+  tab.addEventListener('click', function () {
+    tabs.forEach(el => {
+      el.classList.remove('catalog__tabs-item_active');
+    });
+    this.classList.add('catalog__tabs-item_active');
+    document.querySelectorAll('.catalog__content').forEach(item => {
+      item.classList.remove('catalog__content_active');
+    });
+    document.getElementById(this.dataset.type).classList.add('catalog__content_active');
+  });
+});
